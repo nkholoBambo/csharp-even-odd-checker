1ff922e88ddab83bcacfb6a0e0b28fa27f424908
nkholoBambo <bambomalose423@gmail.com>
2026-03-20 20:47:21 +0200

    Initial commit: Even and Odd number checker

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Choose any number you want and the program will determine whether it\'s a even or odd number");
+double x = Convert.ToDouble(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine($"{x} is an even number");
+}
+
+else
+{
+    Console.WriteLine($"{x} is an odd number");
+}
